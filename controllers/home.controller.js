10081e7b4673e85e9c16256d933f7b60340f648b
Anusara Juntui <j_anusaraone@hotmail.com>
2017-10-07 16:12:55 +0700

    for item+thumbnail

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,3 +1,28 @@
 exports.index = function(req,res) {
-	res.render('home.twig')
+	let content = {
+		highlight: {
+			url:'/assets/images/home/banner/highlight.jpg',
+			title: 'I am Fai'
+		},
+		thumbnail: [
+		{
+			url:'/assets/images/home/banner/thumbnail-1.jpg',
+			title: 'Anusara'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-2.jpg',
+			title: 'Juntui'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-3.jpg',
+			title: 'eiei'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-4.jpg',
+			title: 'tubtui'
+		}
+
+	  ]
+	}
+	res.render('home.twig', content)
 }
